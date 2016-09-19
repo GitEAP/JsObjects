4c28c853e67776ad221d6817b8627e69784b59b3
Erick Perez <NBA_EAP@hotmail.com>
2016-09-19 11:53:20 -0600

    made Robot object

--- Js/objects.js
+++ Js/objects.js
@@ -1,1 +1,25 @@
 //Objects lesson.
+var Robot = {
+    fuel: 100,
+    weapon: "laser",
+    speed: 10,
+    strength: 5,
+    armor: "light",
+
+    addFuel: function (tank) {
+        fuel += tank;
+    },
+
+    upgradeArmor: function (change) {
+        this.armor = change;
+    },
+
+    changeWeapon: function (change) {
+        weapon = change;
+    }
+}
+
+Robot.speed += parseInt(prompt("Increase Robot speed."));
+alert("Our Robot now has a speed of " + Robot.speed);
+Robot.upgradeArmor(prompt("What should we upgrade the armor to?"));
+alert("Our Robot now has " + Robot.armor + " armor.");
